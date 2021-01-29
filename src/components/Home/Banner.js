2011f77478c3880a64d3b594bb99ba970a0a7159
Mustafa Bek <mustafa.bek1995@gmail.com>
2021-01-29 16:51:12 +0300

    commit

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Banner = ({ appName, token }) => {
+  if (token) {
+    return null;
+  }
+  return (
+    <div className="banner">
+      <div className="container">
+        <h1 className="logo-font">
+          {appName.toLowerCase()}
+          
+        </h1> 
+      
+        <p>A Place Where You Can Share Your Feelings.</p>
+        </div>
+      </div>
+  
+  );
+};
+
+export default Banner;
